c0624bf929167df5cb8ab2f953ac4c091baea85a
viswa210 <viswa.s210@gmail.com>
2020-03-22 15:57:29 +0530

    header name and colour change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Food Magic`,
+    description: `Find nearby restaurants you like online using Food Magic`,
+    author: `@viswa210`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
